e02cd9ded39c7f0b3396735b758d3aa0d69ed23b
g14908hh <g14908hh@m20202.tsuda.ac.jp>
2016-05-11 19:32:39 +0900

    testcase

---
+++ snake-camel/test/SnakeCamelTest.java
@@ -0,0 +1,68 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import snakecamel.SnakeCamelUtil;
+
+public class SnakeCamelTest {
+/*
+	@Test
+	public void abc() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="Abc";
+		String act=scu.snakeToCamelcase("abc");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void ab_c_d() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="AbCD";
+		String act=scu.snakeToCamelcase("ab_c_d");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void ab_c＿d() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="AbCD";
+		String act=scu.snakeToCamelcase("ab_c＿d");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void _ab_c() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="AbC";
+		String act=scu.snakeToCamelcase("_ab_c");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void ab__c() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="AbC";
+		String act=scu.snakeToCamelcase("ab__c");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void abcDeF() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="AbcDeF";
+		String act=scu.snakeToCamelcase("abcDeF");
+		assertThat(act, is(ex));
+	}
+	*/
+	@Test
+	public void Abc() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="abc";
+		String act=scu.camelToSnakecase("Abc");
+		assertThat(act, is(ex));
+	}
+	@Test
+	public void AbcDeF() {
+		SnakeCamelUtil scu=new SnakeCamelUtil();
+		String ex="abc_de_f";
+		String act=scu.camelToSnakecase("AbcDeF");
+		assertThat(act, is(ex));
+	}
+
+}
